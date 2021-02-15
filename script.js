04f6f2ffd9d9369dbe51e0b02579b8b999d24296
Gustavo <ltt.gustavo@gmail.com>
2021-02-15 15:39:58 -0300

    Cep - 1

---
+++ script.js
@@ -0,0 +1,48 @@
+function cleanForm() {
+  document.getElementById("rua").value = "";
+  document.getElementById("bairro").value = "";
+  document.getElementById("cidade").value = "";
+  document.getElementById("uf").value = "";
+  document.getElementById("ibge").value = "";
+}
+
+function retorno(conteudo) {
+  if (!("erro" in conteudo)) {
+    document.getElementById("rua").value = conteudo.logradouro;
+    document.getElementById("bairro").value = conteudo.bairro;
+    document.getElementById("cidade").value = conteudo.localidade;
+    document.getElementById("uf").value = conteudo.uf;
+    document.getElementById("ibge").value = conteudo.ibge;
+  } else {
+    cleanForm();
+    alert("CEP não encontrado.");
+  }
+}
+
+function pesquisacep(valor) {
+  var cep = valor.replace(/\D/g, "");
+
+  if (cep != "") {
+    var validacep = /^[0-9]{8}$/;
+
+    if (validacep.test(cep)) {
+      document.getElementById("rua").value = "...";
+      document.getElementById("bairro").value = "...";
+      document.getElementById("cidade").value = "...";
+      document.getElementById("uf").value = "...";
+      document.getElementById("ibge").value = "...";
+
+      var script = document.createElement("script");
+
+      script.src =
+        "https://viacep.com.br/ws/" + cep + "/json/?callback=retorno";
+
+      document.body.appendChild(script);
+    } else {
+      cleanForm();
+      alert("Formato de CEP inválido.");
+    }
+  } else {
+    cleanForm();
+  }
+}
